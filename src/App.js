a40c1fe584230972dd17ed041a211fd7475971b6
Ahmad Koroma <ahmadkoroma25@outlook.com>
2023-12-31 18:51:14 -0500

    DONE!

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, Component } from 'react';
 import axios from 'axios';
-
+import {Routes, Route, Link} from 'react-router-dom';
+// to start working on the routes we're going to need to import  'Routes, Route'
+// components from "react-router-dom". 
+// then create a Routes element in the return statement of the export default 
+// function App.
+import MovieList from './Movies/MovieList'
+// importing MovieList to be used in a Route path as an element
+import Movie from './Movies/Movie'
+// importing Movie Component to be used in second route that will take An
+// id parameter after /movies.
 import SavedList from './Movies/SavedList';
-
 export default function App () {
   const [saved, setSaved] = useState([]); // Stretch: the ids of "saved" movies
   const [movies, setMovies] = useState([]);
@@ -14,11 +22,18 @@ export default function App () {
         .then(response => {
           // Study this response with a breakpoint or log statements
           // and set the response data as the 'movies' slice of state
+          setMovies(response.data)
+          //set Movies passing in response.data
+          // this will make the list on movies load to the page
         })
         .catch(error => {
           console.error('Server Error', error);
         });
     }
+    // An effect that runs after first render, which makes our request
+    // ".get('http://localhost:5001/api/movies')" and once the response comes back
+    //we're supposed to study the response with a breakpoint and set a response data
+    // as the movies slices state ( const [movies, setMovies] = useState([])).
     getMovies();
   }, []);
 
@@ -29,7 +44,21 @@ export default function App () {
   return (
     <div>
       <SavedList list={[ /* This is stretch */]} />
-
+      {/* creating a Routes element and within it a Route which takes the path forward
+      slash and renders an element MovieList. This component will need the movies injected
+      it via props. */}
+      <Routes>
+        {/* At the top of the module, import MovieList */}
+        <Route path="/" element={<MovieList movies={movies} />}/>
+        {/* render MovieList as an element */}
+        {/* dont forget to wrap in the jsx and the movies need to be injected */}
+        {/* the "movies are received after making the get request from getMovies".
+        on success, they need to be shoved into component state and that component 
+        state and that component state is what goes inside of {<MovieList movies={movies} 
+        which is called movies*/}
+        <Route path="movies/:id" element={<Movie />} />
+        {/* second route. the path is going to be movies/id. the element will be Movie */}
+      </Routes>
       <div>Replace this Div with your Routes</div>
     </div>
   );
--- src/Movies/MovieList.js
+++ src/Movies/MovieList.js
@@ -1,10 +1,31 @@
 import React from 'react';
-
+import { useNavigate, Link } from 'react-router-dom';
+// import stuff from react-router-dom and bring the use navigate hook which we will use 
+// inside of export default function MovieList 
 export default function MovieList(props) {
-  return (
+  const navigate = useNavigate()
+  // useNavigate invoked
+  const onMovieClick = id  => () => {
+    // create a click handler const onMovieClick
+    // this needs to take an ID and return a function, 
+    // which takes an event handler
+    // which performs the navigation
+    navigate(`movies/${id}`)
+    // inside of here, we will navigate to movies slash whatever id, 
+    // so we are going to need a template literal, 
+      // movies slash and then interpolate the ID right in there
+
+  }
+    return (
     <div className="movie-list">
       {props.movies.map(movie => (
-        <MovieDetails key={movie.id} movie={movie} />
+        <MovieDetails 
+        link={<Link to={`movies/${movie.id}`}>details</Link>}
+        onMovieClick={onMovieClick(movie.id)}
+         key={movie.id} 
+         movie={movie} />
+        // to send 'onMovieClick' handler down into 
+        // movies details so that it can attach 
       ))}
     </div>
   );
@@ -12,9 +33,9 @@ export default function MovieList(props) {
 
 function MovieDetails(props) {
   const { title, director, metascore } = props.movie;
-
+  const { onMovieClick, link } = props
   return (
-    <div className="movie-card">
+    <div className="movie-card" onClick={onMovieClick}>
       <h2>{title}</h2>
       <div className="movie-director">
         Director: <em>{director}</em>
@@ -22,6 +43,7 @@ function MovieDetails(props) {
       <div className="movie-metascore">
         Metascore: <strong>{metascore}</strong>
       </div>
+      {link}
     </div>
   );
 }
